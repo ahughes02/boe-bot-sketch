24af455ba779d5c5bd0f22dd41857c74b4b4e242
Austin Hughes <ahughes02@live.com>
2014-04-01 16:47:40 -0400

    Partial update to support proximity sensor. Not fully implemented yet

--- robotSketch/robotSketch.ino
+++ robotSketch/robotSketch.ino
@@ -11,6 +11,30 @@ Modified last: 2014-02-04
 
 #include <Servo.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+
+// the i2c address of infrared proximity sensor
+#define VCNL4000_ADDRESS 0x13
+
+// commands and constants for infared proximity sensor
+#define VCNL4000_COMMAND 0x80
+#define VCNL4000_PRODUCTID 0x81
+#define VCNL4000_IRLED 0x83
+#define VCNL4000_AMBIENTPARAMETER 0x84
+#define VCNL4000_AMBIENTDATA 0x85
+#define VCNL4000_PROXIMITYDATA 0x87
+#define VCNL4000_SIGNALFREQ 0x89
+#define VCNL4000_PROXINITYADJUST 0x8A
+
+#define VCNL4000_3M125 0
+#define VCNL4000_1M5625 1
+#define VCNL4000_781K25 2
+#define VCNL4000_390K625 3
+
+#define VCNL4000_MEASUREAMBIENT 0x10
+#define VCNL4000_MEASUREPROXIMITY 0x08
+#define VCNL4000_AMBIENTREADY 0x40
+#define VCNL4000_PROXIMITYREADY 0x20
 
 // servos
 Servo servoRight;	// Declare right servo
@@ -27,6 +51,7 @@ int j = 0; 		// iterator to store data in humidity, temp, and pressure variable
 int exitLoop = 0; 	// variable is set to 1 when valid buffer is stored
 int leftMSint = 1500;   // stores the left microseconds as an integer
 int rightMSint = 1500;  // stores the right microseconds as an integer
+boolean sensor = true;
 
 // References
 // Sample Line of data from serial
@@ -51,6 +76,54 @@ void setup()
   
   servoLeft.attach(13);                     // Attach left signal to pin 13 
   servoLeft.writeMicroseconds(1500);        // 1.5 ms stop
+  
+  Wire.begin();
+  
+   if ((rev & 0xF0) != 0x10) 
+   {
+     Serial.println("Proximity sensor not found");
+     
+     tone(4, 3000, 500);             
+     tone(4, 3000, 1000);                
+     tone(4, 3000, 500);
+     
+     sensor = false;
+   }
+   
+   if(sensor)
+   {
+        write8(VCNL4000_IRLED, 20);        // set to 20 * 10mA = 200mA
+        Serial.print("IR LED current = ");
+        Serial.print(read8(VCNL4000_IRLED) * 10, DEC);
+        Serial.println(" mA");
+        
+        //write8(VCNL4000_SIGNALFREQ, 3);
+        Serial.print("Proximity measurement frequency = ");
+        uint8_t freq = read8(VCNL4000_SIGNALFREQ);
+        if (freq == VCNL4000_3M125) Serial.println("3.125 MHz");
+        if (freq == VCNL4000_1M5625) Serial.println("1.5625 MHz");
+        if (freq == VCNL4000_781K25) Serial.println("781.25 KHz");
+        if (freq == VCNL4000_390K625) Serial.println("390.625 KHz");
+        
+        write8(VCNL4000_PROXINITYADJUST, 0x81);
+        Serial.print("Proximity adjustment register = ");
+        Serial.println(read8(VCNL4000_PROXINITYADJUST), HEX);
+   }
+}
+
+uint16_t readProximity() 
+{
+  write8(VCNL4000_COMMAND, VCNL4000_MEASUREPROXIMITY);
+  while (1) 
+  {
+    uint8_t result = read8(VCNL4000_COMMAND);
+    //Serial.print("Ready = 0x"); Serial.println(result, HEX);
+    if (result & VCNL4000_PROXIMITYREADY) 
+    {
+      return read16(VCNL4000_PROXIMITYDATA);
+    }
+    delay(1);
+  }
 }
 
 void loop() 
