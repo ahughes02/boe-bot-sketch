06e58a9e2bff09e5fae2e849480ca63aa6c9aff1
Austin Hughes <ahughes02@live.com>
2014-04-02 21:48:20 -0400

    I was silly, the Adafruit stuff works fine after these additions

--- robotSketch/robotSketch.ino
+++ robotSketch/robotSketch.ino
@@ -79,7 +79,7 @@ void setup()
   
   Wire.begin();
   
-  uint8_t rev = Wire.read(VCNL4000_PRODUCTID);
+  uint8_t rev = read8(VCNL4000_PRODUCTID);
   
    if ((rev & 0xF0) != 0x10) 
    {
@@ -94,20 +94,20 @@ void setup()
    
    if(sensor)
    {
-        Wire.write(VCNL4000_IRLED, 20);        // set to 20 * 10mA = 200mA
+        write8(VCNL4000_IRLED, 20);        // set to 20 * 10mA = 200mA
         Serial.print("IR LED current = ");
-        Serial.print(Wire.read(VCNL4000_IRLED) * 10, DEC);
+        Serial.print(read8(VCNL4000_IRLED) * 10, DEC);
         Serial.println(" mA");
         
         //write8(VCNL4000_SIGNALFREQ, 3);
         Serial.print("Proximity measurement frequency = ");
-        uint8_t freq = Wire.read(VCNL4000_SIGNALFREQ);
+        uint8_t freq = read8(VCNL4000_SIGNALFREQ);
         if (freq == VCNL4000_3M125) Serial.println("3.125 MHz");
         if (freq == VCNL4000_1M5625) Serial.println("1.5625 MHz");
         if (freq == VCNL4000_781K25) Serial.println("781.25 KHz");
         if (freq == VCNL4000_390K625) Serial.println("390.625 KHz");
         
-        Wire.write(VCNL4000_PROXINITYADJUST, 0x81);
+        write8(VCNL4000_PROXINITYADJUST, 0x81);
         Serial.print("Proximity adjustment register = ");
         Serial.println(read8(VCNL4000_PROXINITYADJUST), HEX);
    }
@@ -115,14 +115,14 @@ void setup()
 
 uint16_t readProximity() 
 {
-  Wire.write(VCNL4000_COMMAND, VCNL4000_MEASUREPROXIMITY);
+  write8(VCNL4000_COMMAND, VCNL4000_MEASUREPROXIMITY);
   while (1) 
   {
-    uint8_t result = Wire.read(VCNL4000_COMMAND);
+    uint8_t result = read8(VCNL4000_COMMAND);
     //Serial.print("Ready = 0x"); Serial.println(result, HEX);
     if (result & VCNL4000_PROXIMITYREADY) 
     {
-      return Wire.read(VCNL4000_PROXIMITYDATA);
+      return read16(VCNL4000_PROXIMITYDATA);
     }
     delay(1);
   }
@@ -217,4 +217,74 @@ void loop()
       servoLeft.writeMicroseconds(leftMSint);
       
       exitLoop = 0;
+}
+
+// Read 1 byte from the VCNL4000 at 'address'
+uint8_t read8(uint8_t address)
+{
+  uint8_t data;
+
+  Wire.beginTransmission(VCNL4000_ADDRESS);
+#if ARDUINO >= 100
+  Wire.write(address);
+#else
+  Wire.send(address);
+#endif
+  Wire.endTransmission();
+
+  delayMicroseconds(170);  // delay required
+
+  Wire.requestFrom(VCNL4000_ADDRESS, 1);
+  while(!Wire.available());
+
+#if ARDUINO >= 100
+  return Wire.read();
+#else
+  return Wire.receive();
+#endif
+}
+
+
+// Read 2 byte from the VCNL4000 at 'address'
+uint16_t read16(uint8_t address)
+{
+  uint16_t data;
+
+  Wire.beginTransmission(VCNL4000_ADDRESS);
+#if ARDUINO >= 100
+  Wire.write(address);
+#else
+  Wire.send(address);
+#endif
+  Wire.endTransmission();
+
+  Wire.requestFrom(VCNL4000_ADDRESS, 2);
+  while(!Wire.available());
+#if ARDUINO >= 100
+  data = Wire.read();
+  data <<= 8;
+  while(!Wire.available());
+  data |= Wire.read();
+#else
+  data = Wire.receive();
+  data <<= 8;
+  while(!Wire.available());
+  data |= Wire.receive();
+#endif
+  
+  return data;
+}
+
+// write 1 byte
+void write8(uint8_t address, uint8_t data)
+{
+  Wire.beginTransmission(VCNL4000_ADDRESS);
+#if ARDUINO >= 100
+  Wire.write(address);
+  Wire.write(data);  
+#else
+  Wire.send(address);
+  Wire.send(data);  
+#endif
+  Wire.endTransmission();
 }
